700aa394b831f16ba0f8e2301da8df77a902b43d
Kádymo Santana <santanakadymo@gmail.com>
2023-11-01 01:30:00 +0000

    fix: Fixing user invocation

--- server/api/user/templates/index.get.ts
+++ server/api/user/templates/index.get.ts
@@ -4,15 +4,21 @@ import { prisma } from "@/server/lib/prisma";
 export default defineEventHandler(async (event) => {
     const session = await getServerSession(event);
 
-    const user = await prisma.user.findUnique({
+    if (!session) {
+        throw createError({
+            status: 400
+        });
+    }
+
+    const user = await prisma.user.findFirst({
         where: {
-            email: session?.user?.email!
+            email: session.user?.email
         }
     });
 
     const templates = await prisma.template.findMany({
         where: {
-            userId: user?.email!
+            userId: user!.id
         }
     });
 
